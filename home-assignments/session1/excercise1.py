8e4b6a59d2e06beb948542f3767d9e0f868cbb91
koomely <koomely@gmail.com>
2019-02-18 20:29:39 +0200

    Finished assignment 1

---
+++ home-assignments/session1/excercise1.py
@@ -0,0 +1,77 @@
+import json
+import yaml
+
+#dict = {}
+#list = []
+
+data = {}
+
+# function open file and set data
+
+def getData():
+    try:
+        with open('sample.json', 'r') as file:
+            data = json.load(file)
+        return data
+    except IOError:
+        print("Can't open file")
+        exit(0)
+
+def writeData(data):
+    try:
+        with open('sample.yml', 'w') as file:
+            yaml.dump(data, file, default_flow_style=False)
+        return 1
+    except IOError:
+        print("Can't open file or file not exist")
+        exit(0)
+
+
+## Generate and split data. Sort buckets list
+
+data = getData()
+
+ppl_ages = data['ppl_ages']
+buckets = data['buckets']
+buckets.sort()
+
+##
+
+final_d = {}
+temp_list = []
+
+buckets_field = []
+
+for i in range(len(buckets)):
+
+    for name,age in ppl_ages.items():
+        if age < buckets[i]:
+            temp_list.append(name)
+# Removing already matched items
+
+    for p in temp_list:
+        if p in ppl_ages:
+            del ppl_ages[p]
+
+    #create strings for dict keys as specified
+
+    if i==0:
+        str='-%d' % buckets[i]
+    else:
+        str='%d-%d' %(buckets[i-1],buckets[i])
+
+    final_d[str] = temp_list.copy()
+    temp_list.clear()
+
+# gathering remaining oldies
+max=0
+for name,age in ppl_ages.items():
+    temp_list.append(name)
+    if age>max:
+        max=age
+
+str='%d-%d' %(buckets[len(buckets)-1], max)
+
+final_d[str] = temp_list.copy()
+
+writeData(final_d)
